6c9df8e8d60baff16d787d2f987d87ccc2e018f7
Willy Xiao <willy@chenxiao.us>
2015-10-17 19:10:40 +0000

    fix csrf

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action do |controller|
-    if not controller.class == 'MainController'
+    if controller.class != MainController
       @text = params.require(:text)
     end
   end
-  
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
 end
